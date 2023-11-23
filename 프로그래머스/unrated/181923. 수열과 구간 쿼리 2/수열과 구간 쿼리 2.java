92c7886da76c502f3ce19212a781b6d08683f292
Sonjiyo <147456475+Sonjiyo@users.noreply.github.com>
2023-11-23 11:03:16 +0900

    [level unrated] Title: 수열과 구간 쿼리 2, Time: 2.49 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        
+        int[] answer = new int[queries.length];
+        
+        for(int i =0; i<answer.length; i++){
+            answer[i] = 1000001;
+        }
+        
+        for(int i =0; i<queries.length; i++){
+            for(int j =queries[i][0]; j<=queries[i][1]; j++){
+                if(arr[j]>queries[i][2]){
+                    if(answer[i]>arr[j]){
+                        answer[i] = arr[j];
+                    }
+                }
+            }
+            if(answer[i] == 1000001){
+                answer[i]=-1;
+            }
+        }
+        return answer;
+    }
+}
