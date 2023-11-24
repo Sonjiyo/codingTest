4e3f3cbea4e925f968ba05805d5a197dbc0292ce
Sonjiyoung <147456475+Sonjiyo@users.noreply.github.com>
2023-11-24 14:54:39 +0900

    [level unrated] Title: 공백으로 구분하기 2, Time: 0.07 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,18 +1,5 @@
 class Solution {
     public String[] solution(String my_string) {
-        String[] arr= my_string.split(" ");
-        int cnt =0;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i].isEmpty()){
-                cnt++;
-            }
-        }
-        String[] answer = new String[arr.length-cnt];
-        for(int i=0, j=0; i<arr.length; i++){
-            if(!arr[i].isEmpty()){
-                answer[j++] = arr[i];
-            }
-        }
-        return answer;
+        return my_string.trim().split("[ ]+");
     }
 }
