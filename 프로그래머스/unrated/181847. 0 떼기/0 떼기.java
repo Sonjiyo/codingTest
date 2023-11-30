4b73a4b7e063989496d239284a220ebbad3526cb
Sonjiyoung <147456475+Sonjiyo@users.noreply.github.com>
2023-11-30 18:28:03 +0900

    [level unrated] Title: 0 떼기, Time: 5.76 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,17 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        String[] arr = n_str.split("");
-        for(int i =0; i<arr.length; i++){
-            if(arr[i].equals("0")){
-                arr[i] = "";
-            } else{
-                break;
-            }
-        }
-        n_str = "";
-        for(String a : arr){
-            n_str += a;
-        }
-        return n_str;
+        return Integer.parseInt(n_str)+"";
     }
 }
