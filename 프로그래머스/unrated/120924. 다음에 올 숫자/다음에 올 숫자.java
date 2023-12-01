0ef5a984121a32da98145d3305a8fc48f3c254ec
Sonjiyoung <147456475+Sonjiyo@users.noreply.github.com>
2023-12-01 12:48:42 +0900

    [level unrated] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(int[] common) {
         int answer = 0;
-        for(int i =0; i<common.length; i++){
-            if(common[1]-common[0] == common[2]-common[1]){
-                answer = common[1]-common[0]+common[common.length-1];
-            } else{
-                answer = common[1]/common[0]*common[common.length-1];
-            }
+        if(common[1]-common[0] == common[2]-common[1]){
+            answer = common[1]-common[0]+common[common.length-1];
+        } else{
+            answer = common[1]/common[0]*common[common.length-1];
         }
         return answer;
     }
