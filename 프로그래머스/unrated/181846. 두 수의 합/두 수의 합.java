74178593110e9592b8c40c5ebe93539168a8693d
Sonjiyoung <147456475+Sonjiyo@users.noreply.github.com>
2023-12-01 11:47:14 +0900

    [level unrated] Title: 두 수의 합, Time: 1161.50 ms, Memory: 382 MB -BaekjoonHub

--- 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/unrated/181846. 두 수의 합/두 수의 합.java
@@ -1,12 +1,28 @@
-import java.math.*;
 class Solution {
     public String solution(String a, String b) {
-        String answer = "";
+        String sum = "";
         
-        BigInteger x = new BigInteger(a);
-        BigInteger y = new BigInteger(b);
-        answer += x.add(y);
-        
-        return answer;
+        int leng = a.length()>b.length() ? a.length() : b.length();
+		int gap = Math.abs(a.length()-b.length());
+		String big = a.length() == leng? a: b;
+		String small = a.length() == leng? b: a;
+
+		int[] total = new int[leng];
+
+		for(int i=leng-1; i>=0; i--) {
+			total[i] += (int)big.charAt(i)-'0';
+			if(gap==0 || i>=gap) {
+				total[i] += (int)small.charAt(i-gap)-'0';
+			}	
+
+			if(i!=0 && total[i]>=10) {
+				total[i] -= 10;
+				total[i-1]+=1;
+			}
+			
+			sum = total[i]+sum;
+		}
+
+        return sum;
     }
 }
