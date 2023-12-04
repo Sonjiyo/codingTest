7d2cca3b6224c4fde85e7ac20b3fc537a8ed4e0e
Sonjiyoung <147456475+Sonjiyo@users.noreply.github.com>
2023-12-04 12:06:04 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,13 +1,11 @@
 class Solution {
     public int solution(int[] date1, int[] date2) {
         int answer = 0;
-        for(int i =0; i<date1.length;){
+        for(int i =0; i<date1.length; i++){
             if(date1[i]>date2[i]) break;
             if(date1[i]<date2[i]){
                 answer = 1;
                 break;
-            } else{
-                i+=1;
             }
         }
         
