0e0b1dcf0e5e3e88076ed3b037ab4b13f5d2fc94
Sonjiyoung <147456475+Sonjiyo@users.noreply.github.com>
2023-12-05 15:08:28 +0900

    [level unrated] Title: 문자열 밀기, Time: 1.20 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.java
@@ -1,28 +1,5 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-        while(true){
-            if(answer==B.length()) {
-                answer = -1;
-                break;
-            }
-           if(A.equals(B)) break;
-           
-           String[] arr = A.split("");
-           
-           String temp = arr[arr.length-1];
-           for(int i =arr.length-1; i>0; i--){
-               arr[i] = arr[i-1];
-           }
-           arr[0] = temp;
-           
-           A = "";
-           for(String a : arr){
-               A += a;
-           }
-           answer++;
-       }
-        
-        return answer;
+        return (B+B).indexOf(A);
     }
 }
