b6b9a2ab5aa12d4dfc2fc59a581df4501bdad55b
Sonjiyoung <147456475+Sonjiyo@users.noreply.github.com>
2024-01-10 15:06:33 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args){
+		System.out.printf("         ,r'\"7\n");
+		System.out.printf("r`-_   ,'  ,/\n");
+		System.out.printf(" \\. \". L_r'\n");
+		System.out.printf("   `~\\/\n");
+		System.out.printf("      |\n");
+		System.out.printf("      |");
+    }
+}
